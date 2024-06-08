cd0d944745401587530728655b00844e63108677
agatamilia <amiliaagata@student.telkomuniversity.ac.id>
2024-06-08 21:05:17 +0700

    Feat : MainApp All In

---
+++ TUBES/Form/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/Tubes', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bufferCommands: false,
+            bufferTimeoutMS: 0,
+        });
+        console.log('MongoDB connected to Tubes database');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
